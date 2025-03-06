6a897c10529e6f9a53b92777eb004013658c5f40
juliastanenko <juliastanenko@gmail.com>
2025-03-06 21:54:34 +0100

    Исправил переменную FIREBASE_SERVICE_ACCOUNT

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,5 +1,9 @@
 const admin = require("firebase-admin");
 
+if (!process.env.FIREBASE_SERVICE_ACCOUNT) {
+  throw new Error("FIREBASE_SERVICE_ACCOUNT is not set in environment variables.");
+}
+
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
