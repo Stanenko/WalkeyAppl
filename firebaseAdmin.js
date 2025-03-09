5c17482134bf3a645a8b4e74d047209fb190b85e
juliastanenko <juliastanenko@gmail.com>
2025-03-09 23:25:49 +0100

    Fix FirebaseAdmin

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -2,7 +2,9 @@ const admin = require("firebase-admin");
 const path = require("path");
 const fs = require("fs");
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+const serviceAccount = JSON.parse(
+  process.env.FIREBASE_CONFIG.replace(/\n/g, "\\n")
+);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
