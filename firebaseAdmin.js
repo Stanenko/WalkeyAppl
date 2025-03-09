cfb6bb895b8eab0d497fd32a22a600e5a70be4a2
juliastanenko <juliastanenko@gmail.com>
2025-03-09 13:56:06 +0100

    Fix: Use FIREBASE_CONFIG from env

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -2,13 +2,7 @@ const admin = require("firebase-admin");
 const path = require("path");
 const fs = require("fs");
 
-const serviceAccountPath = path.join(__dirname, "serviceAccountKey.json");
-
-if (!fs.existsSync(serviceAccountPath)) {
-  throw new Error("Файл serviceAccountKey.json не найден. Убедитесь, что он есть в корне проекта.");
-}
-
-const serviceAccount = require(serviceAccountPath);
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
